cd5cd5f692f8a8159ef5911f73cc0484d6fb5cff
Vincent.VSmeets@gmail.com <Vincent.VSmeets@gmail.com@6422f76b-1567-3a9b-d03d-41e4769984dc>
2012-03-22 21:18:58 +0000

    Moved the project to a maven structure.

---
+++ trunk/RConClient/src/main/java/com/google/rconclient/gui/PopupMenuListener.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.google.rconclient.gui;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPopupMenu;
+
+import com.google.namedlogger.NamedLogger;
+
+/**
+ * A mouse listener to activate a popup menu.
+ * 
+ * @author vincent
+ * 
+ */
+public class PopupMenuListener extends MouseAdapter {
+
+	/**
+	 * The logger for this class.
+	 */
+	private static final NamedLogger LOGGER = new NamedLogger();
+
+	/**
+	 * The popup menu.
+	 */
+	private final JPopupMenu popupMenu;
+
+	/**
+	 * A new listener will be created.
+	 * 
+	 * @param popupMenu
+	 *            The popup menu to show.
+	 */
+	public PopupMenuListener(final JPopupMenu popupMenu) {
+		LOGGER.entering(popupMenu);
+
+		this.popupMenu = popupMenu;
+
+		LOGGER.exiting();
+	}
+
+	@Override
+	public void mousePressed(final MouseEvent event) {
+		LOGGER.entering(event);
+
+		maybeShowPopup(event);
+
+		LOGGER.exiting();
+	}
+
+	@Override
+	public void mouseReleased(final MouseEvent event) {
+		LOGGER.entering(event);
+
+		maybeShowPopup(event);
+
+		LOGGER.exiting();
+	}
+
+	/**
+	 * Show the popup menu in case the event is a popup trigger event.
+	 * 
+	 * @param event
+	 */
+	private void maybeShowPopup(final MouseEvent event) {
+		LOGGER.entering(event);
+
+		if (event.isPopupTrigger()) {
+			popupMenu.show(event.getComponent(), event.getX(), event.getY());
+		}
+
+		LOGGER.exiting();
+	}
+
+}
---
+++ trunk/RConClient/src/main/java/com/google/rconclient/gui/UserListModel.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.google.rconclient.gui;
+
+import java.io.IOException;
+
+import com.google.namedlogger.NamedLogger;
+
+import com.google.rconclient.rcon.AuthenticationException;
+import com.google.rconclient.rcon.RCon;
+
+/**
+ * @author vincent
+ * 
+ */
+public class UserListModel extends AbstractUserListModel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The logger for this class.
+	 */
+	private static final NamedLogger LOGGER = new NamedLogger();
+
+	/**
+	 * @param globals
+	 */
+	public UserListModel(final Globals globals) {
+		super(globals);
+	}
+
+	@Override
+	protected String[] getList(final RCon connection) throws IOException, AuthenticationException {
+		LOGGER.entering(connection);
+
+		final String[] users = connection != null ? connection.list() : new String[0];
+
+		LOGGER.exiting(users);
+		return users;
+	}
+
+}
