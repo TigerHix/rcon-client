cea48c3a7710ab0aa05d154b4fe1c2f6da7b113b
Vincent.VSmeets@gmail.com <Vincent.VSmeets@gmail.com@6422f76b-1567-3a9b-d03d-41e4769984dc>
2012-11-21 21:37:25 +0000

    Removed all the references to the NamedLogger

--- RConClient/src/main/java/com/google/rconclient/gui/KeyEventUtil.java
+++ RConClient/src/main/java/com/google/rconclient/gui/KeyEventUtil.java
@@ -6,8 +6,6 @@ package com.google.rconclient.gui;
 import java.awt.event.KeyEvent;
 import java.lang.reflect.Field;
 
-import com.google.namedlogger.NamedLogger;
-
 /**
  * This class will provide some extra methods in relation to {@link KeyEvent}.
  * 
@@ -17,11 +15,6 @@ import com.google.namedlogger.NamedLogger;
 public abstract class KeyEventUtil {
 
 	/**
-	 * The logger for this class.
-	 */
-	private static final NamedLogger LOGGER = new NamedLogger();
-
-	/**
 	 * Find the key code to the virtual key name.
 	 * 
 	 * @param name
@@ -29,15 +22,11 @@ public abstract class KeyEventUtil {
 	 * @return The key code.
 	 */
 	public static int getKeyCode(final String name) {
-		LOGGER.entering(name);
-
 		if (name == null) {
 			final NullPointerException exception = new NullPointerException("No name defined");
-			LOGGER.throwing(exception);
 			throw exception;
 		} else if (!name.startsWith("VK_")) {
 			final IllegalArgumentException exception = new IllegalArgumentException("Illegal virtual key name: " + name);
-			LOGGER.throwing(exception);
 			throw exception;
 		}
 
@@ -48,20 +37,16 @@ public abstract class KeyEventUtil {
 		} catch (final SecurityException e) {
 			final IllegalArgumentException exception = new IllegalArgumentException("No access allowed to virtual key name: "
 					+ name, e);
-			LOGGER.throwing(exception);
 			throw exception;
 		} catch (final NoSuchFieldException e) {
 			final IllegalArgumentException exception = new IllegalArgumentException("No such virtual key name: " + name, e);
-			LOGGER.throwing(exception);
 			throw exception;
 		} catch (final IllegalAccessException e) {
 			final IllegalArgumentException exception = new IllegalArgumentException("Illegal access to virtual key name: " + name,
 					e);
-			LOGGER.throwing(exception);
 			throw exception;
 		}
 
-		LOGGER.exiting(code);
 		return code;
 	}
 
