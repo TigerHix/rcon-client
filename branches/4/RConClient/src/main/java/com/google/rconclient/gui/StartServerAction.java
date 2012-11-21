9d68e588af642d74bad9b60f743b5c29ef2150f3
Vincent.VSmeets@gmail.com <Vincent.VSmeets@gmail.com@6422f76b-1567-3a9b-d03d-41e4769984dc>
2012-11-21 21:03:35 +0000

    A branch for the issue.

---
+++ branches/4/RConClient/src/main/java/com/google/rconclient/gui/StartServerAction.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.google.rconclient.gui;
+
+import java.awt.event.ActionEvent;
+import java.util.ResourceBundle;
+
+import javax.swing.AbstractAction;
+
+import com.google.namedlogger.NamedLogger;
+
+/**
+ * The action to be used to start the server.
+ * 
+ * @author vincent
+ * 
+ */
+public class StartServerAction extends AbstractAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * The logger for this class.
+	 */
+	private static final NamedLogger LOGGER = new NamedLogger();
+
+	/**
+	 * The resource bundle of the messages.
+	 */
+	private static final ResourceBundle MESSAGES = new Messages(StartServerAction.class);
+
+	private static final String MSG_CLASS_NAME = StartServerAction.class.getSimpleName();
+	private static final String MSG_NAME = MSG_CLASS_NAME + ".name";
+	private static final String MSG_MNEMONIC = MSG_CLASS_NAME + ".mnemonic";
+
+	/**
+	 * The container with the global objects.
+	 */
+	@SuppressWarnings("unused")
+	private final Globals globals;
+
+	/**
+	 * A new action will be created.
+	 * 
+	 * @param globals
+	 *            The container with the global objects.
+	 */
+	public StartServerAction(final Globals globals) {
+		super();
+		LOGGER.entering();
+
+		this.globals = globals;
+		putValue(NAME, MESSAGES.getString(MSG_NAME));
+		putValue(MNEMONIC_KEY, KeyEventUtil.getKeyCode(MESSAGES.getString(MSG_MNEMONIC)));
+
+		LOGGER.exiting();
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent event) {
+		LOGGER.entering(event);
+
+		// TODO
+
+		LOGGER.exiting();
+	}
+
+}
